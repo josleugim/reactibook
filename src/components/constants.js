7eac147ad5ba4c3b9cf6fa568b39cb588c932b31
José Miguel <josleugim@gmail.com>
2020-09-21 11:54:39 -0500

    Staging configuratio

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,4 +1,4 @@
 export const AUTH_TOKEN = 'auth-token';
 export const EMAIL_REGEX = /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i;
-export const API_URL = 'http://localhost:3030';
+export const API_URL = 'https://reactibook-api.df.r.appspot.com';
 export const USER = 'user';
